2490ea6cf7018013f5b65ffe5210f423b75b58cb
Taylor Roebuck <taylornroebuck@gmail.com>
2020-05-12 18:08:20 -0700

    more styling

--- github-usercard/src/components/UserList.js
+++ github-usercard/src/components/UserList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Card, CardHeader, CardTitle, CardImg, CardBody } from 'shards-react';
+import '../App.css';
 
 const UserList = props => {
   console.log('UserList', props);
@@ -8,10 +9,12 @@ const UserList = props => {
       <Card>
         {props.followers.map((follower, id) => (
           <div key={id}>
+            <div className="followers">
               <CardHeader>
               <CardImg src={follower.avatar_url} alt="github follower"/>
               <CardTitle>{follower.login}</CardTitle>
               </CardHeader>
+            </div>
           </div>
         ))}
       </Card>
